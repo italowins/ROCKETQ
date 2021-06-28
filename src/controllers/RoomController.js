7c49590ebe29cc2a7d79956fc55fb7e0a56c1465
italo handerson <italowins@outlook.com>
2021-06-28 15:19:27 -0300

    aula05- 25%

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,22 +5,37 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
 
+        while (isRoom) {
+        //gera o número da sala
         for (var i = 0; i < 6; i++) {
             i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
                 roomId += Math.floor(Math.random() * 10).toString()
         }
+            //verifica se esse número já existe
+            const roomExistIds = await db.all(`SELECT id from rooms`)
 
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        )VALUES(
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
+            isRoom = roomExistIds.some(roomExistIds => roomExistIds === roomId)
+            
+        if(!isRoom){
+            //insere a sala no banco
+            await db.run(`INSERT INTO rooms (
+                id,
+                pass
+            )VALUES(
+                ${parseInt(roomId)},
+                ${pass}
+            )`)
+        }
 
+        }
         await db.close()
-
         res.redirect(`/room/${roomId}`)
+    },
+    async open(req, res){
+        const roomId = req.params.room
+        res.render('room', {roomId: roomId})
+        const questions = await db.all(`SELECT * FROM questions`)
     }
 }
--- src/db/init.js
+++ src/db/init.js
@@ -11,8 +11,9 @@
 
          await db.exec(`CREATE TABLE questions(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            titulo TEXT,
-            read INT
+            title TEXT,
+            read INT,
+            room INT
         )`);
 
          await db.close()
