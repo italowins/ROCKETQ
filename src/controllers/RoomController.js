5ddc1484b68055447311f13ffa880bc388b28cb6
Italo Handerson <italowins@outlook.com>
2021-06-29 14:09:32 -0300

    Projeto 100% Concluído

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -8,34 +8,51 @@ module.exports = {
         let isRoom = true
 
         while (isRoom) {
-        //gera o número da sala
-        for (var i = 0; i < 6; i++) {
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-                roomId += Math.floor(Math.random() * 10).toString()
-        }
+            //gera o número da sala
+            for (var i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                    roomId += Math.floor(Math.random() * 10).toString()
+            }
             //verifica se esse número já existe
             const roomExistIds = await db.all(`SELECT id from rooms`)
 
             isRoom = roomExistIds.some(roomExistIds => roomExistIds === roomId)
-            
-        if(!isRoom){
-            //insere a sala no banco
-            await db.run(`INSERT INTO rooms (
+
+            if (!isRoom) {
+                //insere a sala no banco
+                await db.run(`INSERT INTO rooms (
                 id,
                 pass
             )VALUES(
                 ${parseInt(roomId)},
                 ${pass}
             )`)
-        }
+            }
 
         }
         await db.close()
         res.redirect(`/room/${roomId}`)
     },
-    async open(req, res){
+    async open(req, res) {
+        const db = await Database()
         const roomId = req.params.room
-        res.render('room', {roomId: roomId})
-        const questions = await db.all(`SELECT * FROM questions`)
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+        let isNoQuestions
+
+        if (questions.length == 0) {
+            if (questionsRead.length == 0) {
+                isNoQuestions = true
+            }
+
+        }
+
+        res.render('room', { roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions })
+    },
+
+    enter(req, res) {
+        const roomId = req.body.roomId
+        res.redirect(`/room/${roomId}`)
     }
+
 }
